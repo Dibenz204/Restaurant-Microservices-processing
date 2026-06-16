4fbf61705d2d4a7154cf3f1bf8e41c763f41cb2c
Nguyen Dinh Phong <dinhphong2222004@gmail.com>
2026-06-17 00:03:30 +0700

    Add 3 files of Repository of Auth-service

---
+++ auth-service/src/main/java/com/restaurant/authservice/repository/RefreshTokenRepository.java
@@ -0,0 +1,21 @@
+package com.restaurant.authservice.repository;
+
+import com.restaurant.authservice.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+    Optional<RefreshToken> findByToken(String token);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM RefreshToken rt WHERE rt.user.userId = :userId")
+    void deleteTokensByUserId(Long userId);
+}
